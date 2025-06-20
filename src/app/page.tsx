0ca326723d942c92cf31a2aceea7e0f2d5a12459
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2025-06-19 22:34:11 -0300

    add basic content to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
 export default function Home() {
   return (
-    <div>
-      Hello There
-    </div>
+    <>
+      <header>
+        Jhow Store
+      </header>
+
+      <main>
+        <ul>
+          <li>
+            <a href="/product/1">
+              Product 1
+            </a>
+          </li>
+
+          <li>
+            <a href="/product/2">
+              Product 2
+            </a>
+          </li>
+
+          <li>
+            <a href="/product/3">
+              Product 3
+            </a>
+          </li>
+        </ul>
+      </main>
+
+      <footer>
+        Created by Jhow
+      </footer>
+    </>
   );
 }
