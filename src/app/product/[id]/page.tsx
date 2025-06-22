dc015ba4fb07037856eb6a0bcd5d121508313ca7
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2025-06-21 21:34:04 -0300

    improve product page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,7 +1,27 @@
-export default function Product() {
+import Image from 'next/image'
+
+import data from '../../../data/home.json'
+
+const Product = async ({ params }) => {
+  const { id: productId } = await params
+
+  const product = data.banners.find(p => p.id === Number(productId))
+
+  if (!product) {
+    return (
+      <>
+        <h1>Not Found</h1>
+      </>
+    )
+  }
+  
   return (
     <>
-      <h1>Product</h1>
+      <h1>{product.alt}</h1>
+
+      <Image src={product.src} alt={product.alt} width={1080} height={480} />
     </>
   )
 }
+
+export default Product
