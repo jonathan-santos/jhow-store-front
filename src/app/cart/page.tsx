7fde2203a0855f8e5888e500eed3034229c39b90
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2025-06-25 16:03:52 -0300

    create prototype for cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,6 +1,32 @@
+const cart = [
+  { id: 0, quantity: 2, price: 5.0 },
+]
+
+const getCartTotal = () => {
+  let total = 0;
+
+  cart.forEach(i => {
+    total += i.quantity * i.price
+  })
+
+  return total
+}
+
 const Cart = () => (
   <>
     <h1>Your cart</h1>
+
+    <ul className="items">
+      {cart.map(p => (
+        <li key={p.id} className='item' >
+          $ {p.quantity * p.price}
+        </li>
+      ))}
+    </ul>
+
+    <div>
+      Total: $ {getCartTotal()}
+    </div>
   </>
 )
 
