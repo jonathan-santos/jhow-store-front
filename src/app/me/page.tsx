247a85653ffc08acea5b7378b17470f3e79cd67f
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2025-06-27 09:34:15 -0300

    add list of purchases in me page

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -1,6 +1,24 @@
+import Link from 'next/link'
+
+import home from '../../data/home.json'
+
 const Me = () => (
   <>
     <h1>Me</h1>
+
+    <div>
+      <h2>Purchases</h2>
+
+      <ul>
+        {home.banners.map(b => (
+          <li key={b.id} className='purchase'>
+            <Link href="/product/1">
+              <span>Purchase 1</span>
+            </Link>
+          </li>        
+        ))}
+      </ul>
+    </div>
   </>
 )
 
