b168bc2723dce2c879734eb64dd4570672b2d11a
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2025-07-03 15:23:07 -0300

    create component Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 import "./globals.css";
 
@@ -34,9 +35,7 @@ export default function RootLayout({
           {children}
         </main>
         
-        <footer>
-          Created by Jhow
-        </footer>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => (
+  <footer>
+    Created by Jhow
+  </footer>
+)
+
+export default Footer
