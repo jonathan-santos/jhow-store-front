7e99fcbe485f9909def8bb9923e348099f976a5b
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2025-07-15 19:44:55 -0300

    create data file products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 import home from '../data/home.json'
+import products from '../data/products.json'
 
 import SlideContainer from '@/components/slideContainer/slideContainer'
 import Product from '@/components/product/Product';
@@ -15,7 +16,7 @@ const Home = () => (
     ))}
 
     <SlideContainer>
-      {home.products.map((item) => (
+      {products.slice(0, 9).map((item) => (
         <Link key={item.id} href={`/product/${item.id}`}>
           <Product {...item} />
         </Link>
